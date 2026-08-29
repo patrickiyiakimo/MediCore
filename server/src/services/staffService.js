15945df338c069f664f63c591c06343de412f6b1
Patrick Iyiakimo <137924636+patrickiyiakimo@users.noreply.github.com>
2026-08-29 10:47:06 +0100

    fix: Resolved error

--- server/src/services/staffService.js
+++ server/src/services/staffService.js
@@ -1,7 +1,7 @@
 const staffRepository = require("../repositories/staffRepository");
 
 const listStaff = async ({ role, limit, offset }) =>
-  staffRepository({ role, limit, offset });
+  staffRepository.listStaff({ role, limit, offset });
 
 module.exports = {
   listStaff,
